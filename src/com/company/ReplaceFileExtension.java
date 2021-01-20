f6a5133cfa923690fb4e3223426cee45834d0014
SunnyNami <77606554+SunnyNami@users.noreply.github.com>
2021-01-20 11:19:05 +0100

    v.1

---
+++ src/com/company/ReplaceFileExtension.java
@@ -0,0 +1,32 @@
+package com.company;
+import java.io.File;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ReplaceFileExtension {
+    public static boolean renameFileExtension(String datei, String neueEndung) {
+        String ziel;
+        String aktuelleEndung = getFileExtension(datei);
+
+        if (aktuelleEndung.equals("")) {
+            ziel = datei + "." + neueEndung;
+        }
+        else {
+            ziel = datei.replaceFirst(Pattern.quote("." + aktuelleEndung) + "$", Matcher.quoteReplacement("." + neueEndung));
+        }
+        return new File(datei).renameTo(new File(ziel));
+    }
+
+    public static String getFileExtension(String f) {
+        String ext = "";
+        int i = f.lastIndexOf('.');
+        if (i > 0 && i < f.length() - 1) {
+            ext = f.substring(i + 1);
+        }
+        return ext;
+    }
+
+    public static void main(String args[]) throws Exception {
+        System.out.println(ReplaceFileExtension.renameFileExtension("E:/Testdatei.pdf", "DuHurensohn"));
+    }
+}
