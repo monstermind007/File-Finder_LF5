06721f39655679b7830956c0bb235be1562f61a5
BuildTools <unconfigured@null.spigotmc.org>
2021-01-20 08:24:03 +0100

    Hello New Branch

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,6 @@ import java.security.PublicKey;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello New Branch");
     }
 }
