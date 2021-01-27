f55aa976b5928e6a856d7deec44459aced89a1e3
BuildTools <unconfigured@null.spigotmc.org>
2021-01-27 11:26:53 +0100

    + Check

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,5 +7,7 @@ public class Main {
     public static void main(String[] args){
         Search.lookInAll("C:\\Users\\techc\\OneDrive",".pdf");// Schau was alles existiert und Filtere
         //Kann paar Sekunden dauern (Ab C:\Users\techc\ löbt es bei mir )
+
+        System.out.println(Check.damaged("C:\\test.pdf"));
     }
 }
